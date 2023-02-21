66b1876ff9a6f30c106fe0a33336c87224b33ac7
fabricio covalesci <fabcovalesci@gmail.com>
2023-02-21 14:34:47 -0300

    feat: Configured swagger in the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.getHttpAdapter().getInstance().set('json spaces', 2);
+
+  
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - Loterias Caixa')
+    .setDescription(
+      'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, estando disponível para diversas linguagens e frameworks. Ela gera um site interno no seu backend que descreve, com muitos detalhes, cada endpoint e estrutura de entidades presentes na sua aplicação.',
+    )
+    .setVersion('1.0')
+    .addTag('lotofacil')
+    .addTag('lotomania')
+    .addTag('megasena')
+    .addTag('quina')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+
 }
 bootstrap();
