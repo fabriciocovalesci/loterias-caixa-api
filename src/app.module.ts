a5a54390e021ef206284fb5d01ad9cb33b605a04
fabricio covalesci <fabcovalesci@gmail.com>
2023-02-21 14:46:01 -0300

    feat: Database connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LotteryModule } from './lottery/lottery.module';
 
 @Module({
-  imports: [LotteryModule],
+  imports: [
+    LotteryModule,
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.CONNECTION_STRING,
+      }),
+    }),
+    ConfigModule.forRoot(
+      {
+        isGlobal: true
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
